ac1ab353d7186f9c50d8b33bde574bfd34102562
Simon Burgos <127227455+SimonBIgnacio@users.noreply.github.com>
2023-05-18 15:27:57 -0400

    info

--- Src/liebrary.py
+++ Src/liebrary.py
@@ -19,3 +19,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, classification_report, ConfusionMatrixDisplay
 from sklearn.metrics import confusion_matrix
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.compose import ColumnTransformer
+from sklearn.cluster import DBSCAN
